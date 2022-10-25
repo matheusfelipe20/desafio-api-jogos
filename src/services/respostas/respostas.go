0b22c28f5afffc7d805ecb22cdac095e93086cce
Jonas Victor <jonas.victor@dcx.ufpb.br>
2022-10-25 18:46:17 -0300

    adicionando o package respostas

---
+++ src/services/respostas/respostas.go
@@ -0,0 +1,25 @@
+package respostas
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// JSON retorna uma resposta em JSON para a requisição
+func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
+	w.WriteHeader(statusCode)
+
+	if err := json.NewEncoder(w).Encode(dados); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Erro retorna um erro em formato JSON
+func Erro(w http.ResponseWriter, statusCode int, err error) {
+	JSON(w, statusCode, struct {
+		Erro string `json:"erro"`
+	}{
+		Erro: err.Error(),
+	})
+}
