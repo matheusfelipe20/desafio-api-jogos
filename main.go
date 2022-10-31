ca2ed296fd1599b1952f789699fcce9e3cb3db50
Matheus <felipe_2012matheus@hotmail.com>
2022-10-31 18:37:24 -0300

    reajuste porta

--- main.go
+++ main.go
@@ -1,22 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/matheusfelipe20/projeto-api-jogos/src/controllers"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		panic("Not port")
-	}
-	fmt.Printf("Listening Port %s\n", port)
-
 	r := mux.NewRouter()
 
 	r.HandleFunc("/campeonatos", controllers.ListarCampeonatos).Methods("GET")
@@ -24,7 +16,8 @@ func main() {
 	r.HandleFunc("/cpf", controllers.ListarUsuario).Methods("GET")
 	r.HandleFunc("/", home).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	log.Print("Server started on localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
