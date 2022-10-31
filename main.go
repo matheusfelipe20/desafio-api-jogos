753e090cb2c4bc254f5e8f918c12a1bb19e5395e
Matheus <felipe_2012matheus@hotmail.com>
2022-10-31 18:29:22 -0300

    portas heroku

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/matheusfelipe20/projeto-api-jogos/src/controllers"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		panic("Not port")
+	}
+	fmt.Printf("Listening Port %s\n", port)
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/campeonatos", controllers.ListarCampeonatos).Methods("GET")
@@ -16,8 +24,7 @@ func main() {
 	r.HandleFunc("/cpf", controllers.ListarUsuario).Methods("GET")
 	r.HandleFunc("/", home).Methods("GET")
 
-	log.Print("Server started on localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
